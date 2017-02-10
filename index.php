60d9e21b0690deec26eaa2992d74271fe96802ff
Gamzat Rasulov <rasulov3645@gmail.com>
2017-02-10 22:33:27 +0300

    Скелет готова. Ехали ехала, поехали!

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html> 
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<!-- Òèïà çàãàëîâêè äîêóìåíòà -->
+<head>
+     <meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+     <meta name="author" content="Helsing" />
+     <link href="css/style.css" rel="stylesheet" type="text/css" />
+     <title> Íåíàçâàííûé 4</title>     
+</head>
+
+<!-- Òåëà äîêóìåíòà -->
+<body>
+
+</body>
+
+</html>
